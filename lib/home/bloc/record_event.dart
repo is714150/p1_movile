495c096fbda8161fad2715ecb0dc7f1295cf47f8
is714150 <is714150@iteso.mx>
2022-10-01 23:54:55 -0500

    practica1

--- lib/home/bloc/record_event.dart
+++ lib/home/bloc/record_event.dart
@@ -8,14 +8,6 @@ abstract class RecordEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class OnSendSongFile extends RecordEvent{
-  final Map<String, dynamic> songFileToSave;
-
-  OnSendSongFile({required this.songFileToSave});
-
-  @override
-  List<Object> get props => [songFileToSave];
-}
 
 class LoadSongInfo extends RecordEvent{}
 
